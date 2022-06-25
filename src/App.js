3f9a232df37da1597c52287db569221d91b0d3fd
2du <59404796+vu2872001@users.noreply.github.com>
2022-06-26 02:18:48 +0700

    nav
    category

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import HeaderTop from"./feature/HeaderTop/HeaderTop";
+import Nav from "./feature/Nav/Nav";
 
 
 function App() {
     return (
         <div className="App">
             <HeaderTop />
+            <Nav />
         </div>
     );
 }
