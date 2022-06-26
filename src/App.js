fdd549c3f1fb4b038221087303c5559479352a19
2du <59404796+vu2872001@users.noreply.github.com>
2022-06-26 16:27:30 +0700

    news
    using rss

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import HeaderTop from "./feature/HeaderTop/HeaderTop";
 import Nav from "./feature/Nav/Nav";
 import ArticleHighlight from "./feature/ArticleHighlight/ArticleHighlight.jsx";
 import Highlight from "./feature/Highlight/Highlight";
+import News from "./feature/RecentNews/News";
 function App() {
     return (
         <div className="App">
             <HeaderTop />
             <Nav />
             <Highlight />
+            <News/>
         </div>
     );
 }
